7808f705626436ce120de8ecd204b63dc8b98f98
Justin <justinmckelvey@hotmail.com>
2016-06-04 14:36:54 -0600

    wip: form working, need to update bootstrap layout

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem "font-awesome-rails"
+gem 'simple_form'
+gem 'pry'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
   def index
+    @user = User.new
   end
-  
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        redirect_to root_path, alert: "Your message was sent successfully."
+    else
+        redirect_to root_path, alert: "There was an error sending your message. Please try again or email us directly."
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone, :company, :message)
+  end
+
 end
