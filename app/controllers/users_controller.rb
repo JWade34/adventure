8a505417db3dea2ab0bc683014d4b0c795a3ea61
Justin <justinmckelvey@hotmail.com>
2016-06-09 13:00:30 -0600

    form working with notice message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        redirect_to root_path, alert: "Your message was sent successfully."
+      redirect_to root_path, notice: 'Your message was sent successfully.'
     else
-        redirect_to root_path, alert: "There was an error sending your message. Please try again or email us directly."
+      redirect_to root_path, notice:  "There was an error sending your message. Please try again or email me directly at Adam@adventuremarketing.org" 
     end
   end
 
