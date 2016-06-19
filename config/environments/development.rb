94f357b894c758e631d89bed193e988c3e45eb36
Justin <justinmckelvey@hotmail.com>
2016-06-19 17:53:12 -0600

    action mailer config reset plain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
         :domain               => "gmail.com",
         :user_name            => ENV['gmail_username'],
         :password             => ENV['gmail_password'],
-        :authentication       => 'plain',
+        :authentication       => :plain,
         :enable_starttls_auto => true
   }
 
