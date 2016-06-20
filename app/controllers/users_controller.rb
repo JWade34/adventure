ae17077a0cad8be926ddda4d85ad78d181e23dab
Justin <justinmckelvey@hotmail.com>
2016-06-19 21:37:55 -0600

    smtp config settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      ExampleMailer.sample_email(@user).deliver
+      ExampleMailer.send_email(@user).deliver
       redirect_to root_path, notice: 'Your message was sent successfully.'
     else
       redirect_to root_path, notice:  "There was an error sending your message. Please try again or email me directly at Adam@adventuremarketing.org"
