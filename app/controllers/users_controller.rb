0762183652e04260ee74912f2b730264ba45ab1b
Justin <justinmckelvey@hotmail.com>
2016-07-13 17:16:48 -0600

    change controller action name for mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      ExampleMailer.sample_email(@user).deliver
+      ExampleMailer.send_email(@user).deliver
       ExampleMailer.contact_us(@user).deliver
       redirect_to root_path, notice: 'Your message was sent successfully.'
     else
