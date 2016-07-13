c3ea9cb0b9a0d2fcd923f26887ed7ee56e2bab69
Justin <justinmckelvey@hotmail.com>
2016-07-13 17:09:55 -0600

    add email to adam when contact form is filled out

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -6,4 +6,9 @@ class ExampleMailer < ActionMailer::Base
     mail(to: @user.email, subject: 'Lets start the ADventure!')
   end
 
+  def contact_us(user)
+    @user = user
+    mail(to: 'justinwmckelvey@gmail.com', subject: 'A New Contact Form has been submitted')
+  end
+
 end
