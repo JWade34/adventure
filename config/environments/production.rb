753526dfb2ca0152b44096e2d0de9b6e6434a0b7
Justin <justinmckelvey@hotmail.com>
2016-07-13 18:20:11 -0600

    smtp production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,8 @@ Rails.application.configure do
       :user_name            => ENV['gmail_username'],
       :password             => ENV['gmail_password'],
       :authentication       => :plain,
-      :enable_starttls_auto => true
+      :enable_starttls_auto => true,
+      :openssl_verify_mode  => 'none'
   }
 
 
